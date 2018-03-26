b71ec98169a2bc8179fd84d10fec710ef6ef8d2b
dratler <dratler.shay@gmail.com>
2018-03-26 22:15:25 +0300

    adding test cases for GET

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,35 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {todoModel} = require('./../model/todo');
 
+const mockToDoData = [{text:'dummy Data one'},{text:'dummy Data two'}];
+
 beforeEach((done) => {
-    todoModel.remove({}).then(() => done());
+    todoModel.remove({}).then(() => {
+       return todoModel.insertMany(mockToDoData);
+    }).then(()=> done());
 });
+
+describe('GET /todo', () => {
+  it('should get all the task at the system',(done)=>{
+    request(app)
+      .get('/todo')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body['todos']).toBe(mockToDoData);
+      }).end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        todoModel.find().then((todos) => {
+          console.log(`here ???? ${todos}`);
+          expect(todos['todos'].length).toBe(mockToDoData.length());
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
 
 describe('POST /todo', () => {
   it('should create a new todo', (done) => {
@@ -24,7 +50,7 @@ describe('POST /todo', () => {
           return done(err);
         }
 
-        todoModel.find().then((todos) => {
+        todoModel.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +69,10 @@ describe('POST /todo', () => {
         }
 
         todoModel.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(mockToDoData.length);
           done();
-        }).catch((e) => done(e));
+        }).catch((e) => done('jjjj'+e));
       });
   });
 });
+
