3aac4b06cb59339f88a9e922ab34a3512de018da
dratler <dratler.shay@gmail.com>
2018-04-18 09:20:51 +0300

    set for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/tododb');
+let mongo_uri = (process.env.MONGO_URI) ? 'mongodb://db_user_one:ZAQ!xsw2@ds147459.mlab.com:47459/node_c_todo_db':'mongodb://localhost:27017/tododb';
+mongoose.connect(mongo_uri);
 
 module.exports = {mongoose};
