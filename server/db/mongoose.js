74074b58eea54bd8f0001dfe94766e495b6c6d7a
dratler <dratler.shay@gmail.com>
2018-04-19 23:57:24 +0300

    port issue

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let mongo_uri = (process.env.PORT) ? 'mongodb://db_user_one:q1w2e3r4t5y6u7@ds147459.mlab.com:47459/node_c_todo_db' : 'mongodb://localhost:27017/tododb' 
-mongoose.connect();
+let mongo_uri = (process.env.PORT) ? 'mongodb://db_user_one:q1w2e3r4t5y6u7@ds147459.mlab.com:47459/node_c_todo_db' :'mongodb://localhost:27017/tododb'; 
+mongoose.connect(mongo_uri);
 
 module.exports = {mongoose};
