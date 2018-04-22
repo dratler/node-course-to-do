76241d974779507e5f5e129024d96c5fadc4c692
dratler <dratler.shay@gmail.com>
2018-04-22 23:08:25 +0300

    adding config

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const config = require('./config/config');
 const express = require('express');
 const body = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -6,7 +7,7 @@ const {mongoose} = require('../server/db/mongoose');
 const {todoModel} = require('./model/todo');
 const {userModel} = require('./model/user');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 let app = express();
 
 app.use(body.json());
